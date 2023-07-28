bbfd17e8daedb2b8cc9d4b3d44da0e208e74abe4
nevilia <winter04nevilia@gmail.com>
2023-07-28 12:30:00 +0530

    tailwind error

--- chatapp/chatapp/settings.py
+++ chatapp/chatapp/settings.py
@@ -28,19 +28,20 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 LOGOUT_REDIRECT_URL = '/'
-LOGIN_REDIRECT_URL = 'rooms'
+LOGIN_REDIRECT_URL = '/rooms/'
 LOGIN_URL = '/login/'
 
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'channels', 
+    # 'channels',
     'core',
     'room',
 ]
@@ -76,6 +77,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'chatapp.wsgi.application'
 ASGI_APPLICATION = 'chatapp.asgi.application'
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer'
+    }
+}
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
